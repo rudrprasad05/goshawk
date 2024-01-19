d6e34448843907ee992b59da6139f8377e084ea7
Rudr <rudrprasad@yahoo.com>
2024-01-19 20:34:31 +1200

    bug: router used in server component

--- app/(seller)/seller/products/details/page.tsx
+++ app/(seller)/seller/products/details/page.tsx
@@ -1,6 +1,4 @@
 import RedirectPage from "@/components/Admin/products/RedirectPage";
-import { useRouter } from "next/navigation";
-import React, { useEffect } from "react";
 
 const page = () => {
   return <RedirectPage />;
